bc58a07c778293bd76729ae9ff2de082358bda9a
hyk3404 <cow3404@gmail.com>
2019-05-24 05:55:51 +0800

    recommend4

--- recommender.py
+++ recommender.py
@@ -6,31 +6,36 @@ def recommend(music_path,music_name): #(mp3位置,mp3名稱)都吃list
     # cos_list, cos_name, folder_list, folder_name = file_path('./dataset', '_cos.csv')
     # euc_list, euc_name, folder_list, folder_name = file_path('./dataset', '_euc.csv')
     recommend_list=[]
+    clean_name = []
+
+    for i in music_name:
+        clean_name.append(i.replace('.mp3', ''))
+
     for mp3_path in music_path:
-        cos = pd.read_csv(mp3_path.replace('.npy', '_cos.csv'))
+        cos = pd.read_csv(mp3_path.replace('.mp3', '_cos.csv'))
         cos = cos.iloc[1:,0:1].values.tolist()
 
         for i in range(len(cos)):
             cos[i]=cos[i][0]
 
-        euc = pd.read_csv(mp3_path.replace('.npy', '_euc.csv'))
+        euc = pd.read_csv(mp3_path.replace('.mp3', '_euc.csv'))
         euc = euc.iloc[1:,0:1].values.tolist()
 
         for i in range(len(cos)):
             euc[i]=euc[i][0]
 
         ret = list(set(cos).intersection(set(euc)))
-        ret = list(set(ret).difference(set(music_name)))
+        ret = list(set(ret).difference(set(clean_name)))
 
         if len(ret)>=2:
             ret = ret[0:2]
 
         elif len(ret)==1:
-            euc_set = list(set(euc).difference(set(music_name)))
+            euc_set = list(set(euc).difference(set(clean_name)))
             ret = ret + euc_set[0]
         
         else:
-            euc_set = list(set(euc).difference(set(music_name)))
+            euc_set = list(set(euc).difference(set(clean_name)))
             ret = ret[0:2]
 
         recommend_list += ret
